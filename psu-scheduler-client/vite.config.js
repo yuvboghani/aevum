72768efb0887f85564b216268ad38d7ed7143535
yuvboghani <yuvboghani@gmail.com>
2026-01-26 19:57:49 -0500

    Config: Update base path to /aevum/home and fix callback rewrites

--- psu-scheduler-client/vite.config.js
+++ psu-scheduler-client/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aevum/', // Subpath deployment on yuvboghani.com/aevum
+  base: '/aevum/home/', // Subpath deployment on yuvboghani.com/aevum/home
 })
 
